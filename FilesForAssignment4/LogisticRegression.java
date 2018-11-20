42cf2043c5012742591a1335661bd0dbb7cf4f5a
pjohn1203 <33132277+pjohn1203@users.noreply.github.com>
2018-11-19 17:19:40 -0800

    modified predict

--- FilesForAssignment4/LogisticRegression.java
+++ FilesForAssignment4/LogisticRegression.java
@@ -83,7 +83,10 @@ public class LogisticRegression {
             int result = (int) sum;
             return result;
             **/
-            
+
+            double x = probPred1(x);
+            if(x > 0.5) return 1;
+            else return 0;
         }
 
         /** This function takes a test set as input, call the predict() to predict a label for it, and prints the accuracy, P, R, and F1 score of the positive class and negative class and the confusion matrix **/
